dfb2fdb1ae08a14dcfac221b1b875b06d41a1dc0
Sasha <alexander.drozd@gmail.com>
2020-02-18 14:01:29 +0900

    explicit float32

--- simple_kernels/conv_torch.py
+++ simple_kernels/conv_torch.py
@@ -7,10 +7,10 @@ from timeit import default_timer as timer
 
 # TODO: consider calling functional directly
 #result = F.conv2d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1)
-torch.backends.mkldnn.enabled = False
+torch.backends.mkldnn.enabled = True
 
 cnt_channels = 3
-size_image = 456
+size_image = 512
 cnt_filters = 32
 size_kernel = 3
 size_batch = 32
@@ -30,7 +30,7 @@ conv1 = torch.nn.Conv2d(in_channels=cnt_channels,
 np_random = np.ones((size_batch,
                      cnt_channels,
                      size_image,
-                     size_image)).astype(np.float)
+                     size_image)).astype(np.float32)
 
 tensor_input = torch.Tensor(np_random)
 time_start = timer()
