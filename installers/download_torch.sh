de9f49568b6bcde71f182aec1f021d3fb8d49182
Emil VATAI <emil.vatai@riken.jp>
2020-08-26 23:45:49 +0900

    Fix download_torch.sh (cd dl path, rm old)

--- installers/download_torch.sh
+++ installers/download_torch.sh
@@ -2,13 +2,16 @@
 
 . common.sh
 
+cd ${DOWNLOAD_PATH}
 # download_torch.sh
+rm -rf dnnl_aarch64
 git clone https://github.com/fujitsu/dnnl_aarch64.git
 cd dnnl_aarch64/
 git submodule sync
 git submodule update --init --recursive
 cd ../
 
+rm -rf pytorch
 git clone https://github.com/pytorch/pytorch.git
 cd pytorch
 git checkout -b v1.5.0 refs/tags/v1.5.0
