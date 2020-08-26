ec3b69c266b12dd0561803f0ba1d475dbfc72d8c
Emil VATAI <emil.vatai@riken.jp>
2020-08-26 23:16:39 +0900

    Add missing download_torch.sh from all.sh

--- installers/all.sh
+++ installers/all.sh
@@ -2,4 +2,5 @@
 rm -rf .local/ down slurm-*
 ./download_python.sh || exit 1
 PYTHON_JOB_ID=$(sbatch install_python.sh) || exit 2
-TORCH_JOB_ID=$(sbatch -d afterok:${PYTHON_JOB_ID##* } install_torch.sh) || exit 3
+./download_torch.sh || exit 3
+TORCH_JOB_ID=$(sbatch -d afterok:${PYTHON_JOB_ID##* } install_torch.sh) || exit 4
