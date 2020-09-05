cd659d94323dc424b3a4daeb6f69b1aecf1000f7
Emil VATAI <emil.vatai@riken.jp>
2020-09-05 19:15:43 +0900

    Add jpeg and Pillow compilation

--- installers/install_vision.sh
+++ installers/install_vision.sh
@@ -5,6 +5,17 @@
 cd ${PYTORCH_INSTALL_PATH}/${VENV_NAME}
 source bin/activate
 
+cd ${DOWNLOAD_PATH}
+cd jpeg-9d/
+./configure --prefix="${PREFIX}/.local" --enable-shared
+make clean
+make -j$(nproc)
+make install
+
+cd ${DOWNLOAD_PATH}
+cd Pillow
+MAX_CONCURRENCY=8 CFLAGS="-I${PREFIX}/.local/include" python3 setup.py install
+
 cd ${DOWNLOAD_PATH}/vision
 python3 setup.py clean
 python3 setup.py install
