b87974bb6bdf7cf3206a7bfe506cdc2092f871b5
Emil VATAI <emil.vatai@riken.jp>
2020-09-09 13:40:46 +0900

    Add activate script

---
+++ installers/activate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This file should be SOURCED, not executed!
+# . /path/to/activate.sh
+
+ABSPATH=$(dirname $(readlink -f $BASH_SOURCE))
+export LD_LIBRARY_PATH=$ABSPATH/.local/lib/ 
+module load system/fx700 
+module load FJSVstclanga/1.1.0
+. $ABSPATH/fccbuild_v150/bin/activate
+
