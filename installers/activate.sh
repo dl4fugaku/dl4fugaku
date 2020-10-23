aa301d5c2a52016b087c416dedffa3a19d1ecb7e
Emil VATAI <emil.vatai@riken.jp>
2020-10-23 23:13:28 +0900

    Fix `activate.sh`

--- installers/activate.sh
+++ installers/activate.sh
@@ -2,7 +2,7 @@
 # . /path/to/activate.sh
 
 ABSPATH=$(dirname $(readlink -f $BASH_SOURCE))
-export LD_LIBRARY_PATH=$ABSPATH/.local/lib/ 
+export LD_LIBRARY_PATH=$ABSPATH/.local/lib/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 module load system/fx700 
 module load FJSVstclanga/1.1.0
 . $ABSPATH/fccbuild_v150/bin/activate
