1c87ea55e6b21e1ad236d10e43300a965f7c1b7f
Emil VATAI <emil.vatai@gmail.com>
2020-11-09 21:52:14 +0900

    Update/fix `activate.sh`

--- installers/activate.sh
+++ installers/activate.sh
@@ -1,16 +1,16 @@
 # This file should be SOURCED, not executed!
 # . /path/to/activate.sh
 
-module purge
 ABSPATH=$(dirname $(readlink -f $BASH_SOURCE))
 
 export VENV_PATH=${ABSPATH}/venv
 export PERFIX=${ABSPATH}/opt
 export PATCH_DIR=${ABSPATH}/up
 
-export LD_LIBRARY_PATH=$PREFIX/lib/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+module purge
 module load system/fx700
 module load FJSVstclanga/1.1.0
+export LD_LIBRARY_PATH=$PREFIX/lib/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 [ -d ${VENV_PATH} ] && \
     source ${VENV_PATH}/bin/activate || \
