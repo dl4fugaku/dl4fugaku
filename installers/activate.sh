84d74dbcbb797b7dc77e308147410066aa3c096f
Emil VATAI <emil.vatai@riken.jp>
2020-11-11 07:47:05 +0900

    Make explicit if in `activate.sh`

--- installers/activate.sh
+++ installers/activate.sh
@@ -12,6 +12,8 @@ module load system/fx700
 module load FJSVstclanga/1.1.0
 export LD_LIBRARY_PATH=$PREFIX/lib/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
-[ -d ${VENV_PATH} ] && \
-    source ${VENV_PATH}/bin/activate || \
+if [ -d "${VENV_PATH}" ]; then
+    source ${VENV_PATH}/bin/activate
+else
     echo "Warning: venv non-existing (if you are installing python, it is ok!)"
+fi
