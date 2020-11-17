9509ba77be05d8dc01709017a457e9703e0f409f
Emil VATAI <emil.vatai@riken.jp>
2020-11-17 16:07:32 +0900

    Fix last commit, add "follow links" switch

--- installers/install_torch.sh
+++ installers/install_torch.sh
@@ -39,5 +39,5 @@ ln -sf xed-install-base-* xed
 
 cd ${DOWNLOAD_PATH}/pytorch
 python setup.py install
-cp ${DOWNLOAD_PATH}/pytorch/third_party/ideep/mkl-dnn/third_party/build_xed_aarch64/kits/xed/lib/libxed.so ${PREFIX}/lib/libxed.so
+cp -L ${DOWNLOAD_PATH}/pytorch/third_party/ideep/mkl-dnn/third_party/build_xed_aarch64/kits/xed/lib/libxed.so ${PREFIX}/lib/libxed.so
 
