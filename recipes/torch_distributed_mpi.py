7e58f414983dcc287321409dc648987e70532694
Emil VATAI <emil.vatai@gmail.com>
2021-05-31 19:53:14 +0900

    Cleanup

--- recipes/torch_distributed_mpi.py
+++ recipes/torch_distributed_mpi.py
@@ -3,13 +3,12 @@
 import torch.distributed as dist
 
 if __name__ == "__main__":
-    available = dist.is_mpi_available()
-    print(f"mpi available: {available}")
     if dist.is_mpi_available():
         backend = "mpi"
     elif dist.is_nccl_available():  # and gpus
         backend = "nccl"
     elif dist.is_gloo_available():
         backend = "gloo"
+    print(f"using backend: {backend}")
     dist.init_process_group(backend)
     print(f"RANK: {dist.get_rank()}, SIZE: {dist.get_world_size()}")
