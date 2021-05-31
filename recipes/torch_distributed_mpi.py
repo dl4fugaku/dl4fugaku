511408cb9e2114624558be8c67c92aa6cc63079e
Emil VATAI <emil.vatai@gmail.com>
2021-05-31 19:19:04 +0900

    Add missing parenthesis

--- recipes/torch_distributed_mpi.py
+++ recipes/torch_distributed_mpi.py
@@ -5,5 +5,5 @@ import torch.distributed as dist
 if __name__ == "__main__":
     available = dist.is_mpi_available()
     print(f"mpi available: {available}")
-    dist.init_process_group("mpi" if dist.is_mpi_available else "env://")
+    dist.init_process_group("mpi" if dist.is_mpi_available() else "env://")
     print(f"RANK: {dist.get_rank()}, SIZE: {dist.get_world_size()}")
