89e4e83698ff5e7c06f93cb3eb9cfdbe7e4e4914
Emil VATAI <emil.vatai@gmail.com>
2021-05-31 19:36:04 +0900

    Fancy backends

--- recipes/torch_distributed_mpi.py
+++ recipes/torch_distributed_mpi.py
@@ -5,6 +5,11 @@ import torch.distributed as dist
 if __name__ == "__main__":
     available = dist.is_mpi_available()
     print(f"mpi available: {available}")
-    backend = "mpi" if dist.is_mpi_available() else "nccl"
+    if dist.is_mpi_available():
+        backend = "mpi"
+    elif dist.is_nccl_available():  # and gpus
+        backend = "nccl"
+    elif dist.is_gloo_available():
+        backend = "gloo"
     dist.init_process_group(backend)
     print(f"RANK: {dist.get_rank()}, SIZE: {dist.get_world_size()}")
