5a5be4185c05177e9e63857ccc474a2bd1a684f3
Emil VATAI <emil.vatai@gmail.com>
2021-05-31 19:34:03 +0900

    Make nccl backend

--- recipes/torch_distributed_mpi.py
+++ recipes/torch_distributed_mpi.py
@@ -5,8 +5,6 @@ import torch.distributed as dist
 if __name__ == "__main__":
     available = dist.is_mpi_available()
     print(f"mpi available: {available}")
-    if dist.is_mpi_available():
-        dist.init_process_group("mpi")
-    else:
-        dist.init_process_group()
+    backend = "mpi" if dist.is_mpi_available() else "nccl"
+    dist.init_process_group(backend)
     print(f"RANK: {dist.get_rank()}, SIZE: {dist.get_world_size()}")
