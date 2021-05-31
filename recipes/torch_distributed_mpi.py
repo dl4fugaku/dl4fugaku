2aeab38417494bc5a4eff5f4a40af8ca1f6288e6
Emil VATAI <emil.vatai@gmail.com>
2021-05-31 19:22:26 +0900

    Branch for "mpi" backend

--- recipes/torch_distributed_mpi.py
+++ recipes/torch_distributed_mpi.py
@@ -5,5 +5,8 @@ import torch.distributed as dist
 if __name__ == "__main__":
     available = dist.is_mpi_available()
     print(f"mpi available: {available}")
-    dist.init_process_group("mpi" if dist.is_mpi_available() else "env://")
+    if dist.is_mpi_available:
+        dist.init_process_group("mpi")
+    else:
+        dist.init_process_group()
     print(f"RANK: {dist.get_rank()}, SIZE: {dist.get_world_size()}")
