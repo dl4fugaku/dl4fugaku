28f627a3187fe5fce716499c5cb6bb4f8d87077c
Emil Vatai <emil.vatai@gmail.com>
2023-02-02 09:57:32 +0900

    Update fugaku_submit.sh

--- recipes/fugaku_submit.sh
+++ recipes/fugaku_submit.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-#PJM -L "rscunit=rscunit_ft01,rscgrp=small"
+#PJM -L "rscgrp=small"
 #PJM -L elapse=00:01:00
 #PJM -L "node=2"
-#PJM --mpi "proc=4"
+#PJM --mpi "proc=8"
 #PJM -j
 #PJM -S
 
-# RUN IT WITH:
-# $ pjsub magic.sh
+# rscgrp=large for more nodes
+# elapse HH:MM:SS
+# Process per node (PPN) is P/N where `--mpi "proc=P"` and `-L "node=N"`
+# -j merges stdout and stderr
+# -S generates statistics report (after job completion)
 
+# RUN IT WITH:
+# $ pjsub fugaku_submit.sh
+
+# pytorch 1.10 build instructions are available
 export PATH=/home/apps/oss/PyTorch-1.7.0/bin:$PATH
 export LD_LIBRARY_PATH=/home/apps/oss/PyTorch-1.7.0/lib:$LD_LIBRARY_PATH
-LD_PRELOAD=libtcmalloc.so mpirun -n $PJM_MPI_PROC python3 torch_distributed.py
-
+LD_PRELOAD=libtcmalloc.so \
+  mpirun python3 torch_distributed.py
